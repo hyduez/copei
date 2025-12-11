63eb2cffecfb1b7b0ce4c6e690fba38937ac5cc8
Paulo Viteri <undevoted@proton.me>
2025-12-11 09:33:57 -0500

    style(format): biome ahh checks

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Healthy vs. Junk';
-export const SITE_DESCRIPTION = "We're from  AE10's course of cycle I118, and we're presenting on fast food vs. healthy food. We're tired of the same old narrative of “eat this, not that.”";
+export const SITE_DESCRIPTION =
+  "We're from  AE10's course of cycle I118, and we're presenting on fast food vs. healthy food. We're tired of the same old narrative of “eat this, not that.”";
