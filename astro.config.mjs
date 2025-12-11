184c5dfa205282b6b09b14444f7543e620f95c4f
Paulo Viteri <undevoted@proton.me>
2025-12-11 07:23:13 -0500

    feat(deps): install tailwind and remove rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import icon from 'astro-icon';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), icon()],
+  vite: {
+    // @ts-expect-error
+    plugins: [tailwindcss()],
+  },
 });
