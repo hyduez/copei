7c6583d9ce5e3e54026c9862baba4f7cd6637b25
Paulo Viteri <undevoted@proton.me>
2025-12-11 12:03:04 -0500

    chore: few changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://copei-edu.vercel.app',
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     // @ts-expect-error
