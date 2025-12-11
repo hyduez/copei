ea70e77a5ede50be7f9f2f1ab14c8755192645a2
Paulo Viteri <undevoted@proton.me>
2025-12-11 09:08:29 -0500

    chore: update some critical errors on the codebase

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Eat Well, Live Well';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Healthy vs. Junk';
+export const SITE_DESCRIPTION = "We're from  AE10's course of cycle I118, and we're presenting on fast food vs. healthy food. We're tired of the same old narrative of “eat this, not that.”";
